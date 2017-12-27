d7506c34165c5651902228b08b983670d5edf309
Manish Kumar <ManishK@talentica-all.com>
2017-12-27 11:54:16 +0530

    Updated file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', routes.home);
 app.get('/:city', routes.city);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var server = app.listen(port, function (req, res) {
     console.log("catch the action at http://localhost:" + port);
 }
